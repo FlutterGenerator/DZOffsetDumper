86b6d52f37f41c30094f7a24d954b9bd92a37594
tojik_proof_93 <elementman50@yandex.com>
2026-03-24 10:11:02 +0500

    java 1.8

--- app/src/main/java/com/dzimmortal/Offsetdummper/FloatingModMenuService.java
+++ app/src/main/java/com/dzimmortal/Offsetdummper/FloatingModMenuService.java
@@ -2,8 +2,6 @@
 
 package com.dzimmortal.Offsetdummper;
 
-import android.animation.ValueAnimator;
-import android.annotation.TargetApi;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningAppProcessInfo;
 import android.app.AlertDialog;
